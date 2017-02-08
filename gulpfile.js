fc6ecb66d2cda486caa876dd8fa6510236400dff
Ivan Rodriguez <frivanrodriguez@gmail.com>
2017-02-08 12:46:20 +0100

    Import mobx as ES6 import & remove from it polobx.html

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,26 @@
 const gulp = require('gulp');
-const rollup = require('gulp-better-rollup')
-const babel = require('rollup-plugin-babel')
+const rollup = require('gulp-better-rollup');
+const babel = require('rollup-plugin-babel');
+const nodeResolve = require('rollup-plugin-node-resolve');
+const commonjs = require('rollup-plugin-commonjs');
 const rename = require("gulp-rename");
 
 gulp.task('default', () =>
     gulp.src('src/index.js')
         .pipe(rollup({
-          plugins: [babel()],
+          plugins: [
+            babel(),
+            nodeResolve({
+              jsnext: true,
+              main: true
+            }),
+            commonjs({
+              include: 'node_modules/**',
+              extensions: [ '.js', '.coffee' ],
+              ignoreGlobal: false,
+              sourceMap: true
+            })
+          ],
           moduleName: 'createPolobxBehavior'
         }, 'umd'))
         .pipe(rename('polobx.js'))
