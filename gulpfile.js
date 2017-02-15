521b699d2e450fc3367558cf0e927499d043616a
Ivan Rodriguez <frivanrodriguez@gmail.com>
2017-02-15 16:42:23 +0100

    Add release tasks & scripts

--- gulpfile.js
+++ gulpfile.js
@@ -4,8 +4,17 @@ const babel = require('rollup-plugin-babel');
 const nodeResolve = require('rollup-plugin-node-resolve');
 const commonjs = require('rollup-plugin-commonjs');
 const rename = require("gulp-rename");
+const bump = require("gulp-bump");
+const args = require('yargs').argv;
+const git = require('gulp-git');
+const fs = require('fs');
+const runSequence = require('run-sequence');
 
 require('web-component-tester').gulp.init(gulp);
+
+const getPackageJson = function () {
+  return JSON.parse(fs.readFileSync('./package.json', 'utf8'));
+};
 
 gulp.task('default', () =>
     gulp.src('src/index.js')
@@ -28,3 +37,35 @@ gulp.task('default', () =>
         .pipe(rename('polobx.js'))
         .pipe(gulp.dest('dist'))
 );
+
+gulp.task('commit-release', () => {
+  const { version } = getPackageJson();
+
+  console.warn('-----> Remember to push commit and publish the package <------');
+
+  return gulp.src('./git/*')
+    .pipe(git.add({args: '.'}))
+    .pipe(git.commit(`Bump version: ${version}`))
+    .pipe(git.tag(version, version, err => {
+      if (err) throw err;
+    }));
+});
+
+gulp.task('release', () => {
+
+  const type = args.type;
+  const options = {
+    type
+  };
+
+  return gulp.src(['./package.json', './bower.json'])
+  .pipe(bump(options))
+  .pipe(gulp.dest('./'));
+});
+
+gulp.task('bump', callback => {
+  runSequence(
+    ['default', 'release'],
+    'commit-release',
+    callback);
+});
